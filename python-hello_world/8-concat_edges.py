3f3841478ba91f8db09e766f84e32a01b8a142f1
Joleen <12113@holbertonstudents.com>
2026-01-20 15:32:27 +1100

    Task 8

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+phrase = [
+    "Python", "is", "an", "interpreted,",  "interactive,",
+    "object-oriented", "programming", "language", "that", "combines",
+    "remarkable", "power", "with", "very", "clear", "syntax"
+]
+print(phrase[5], phrase[6], phrase[12], phrase[0])
