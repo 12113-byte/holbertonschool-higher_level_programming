a1e7951c26e46fc1569b88bb9742f2c58a2f4919
Joleen <12113@holbertonstudents.com>
2026-01-20 15:33:18 +1100

    Pycodestyle changes

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-phrase = [
-    "Python", "is", "an", "interpreted,",  "interactive,",
+phrase = [ "Python", "is", "an", "interpreted,",  "interactive,",
     "object-oriented", "programming", "language", "that", "combines",
-    "remarkable", "power", "with", "very", "clear", "syntax"
-]
+    "remarkable", "power", "with", "very", "clear", "syntax" ]
 print(phrase[5], phrase[6], phrase[12], phrase[0])
