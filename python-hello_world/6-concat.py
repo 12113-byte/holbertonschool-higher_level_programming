7955b68bfdbc2b364131cf59c6b1ef533c2e0ae2
Joleen <12113@holbertonstudents.com>
2026-01-20 14:56:11 +1100

    Pycodestyle fix

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton" #assign str1
-str2 = "School" #assign str2
+str1 = "Holberton"  # assign str1
+str2 = "School"  # assign str2
 print(f"Welcome to {str1} {str2}!")
-
