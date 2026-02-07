e8c10452f2fb0858092875e38e411cc84c426f34
Joleen <12113@holbertonstudents.com>
2026-02-07 18:44:24 +1100

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module defining a class called Square."""
+
+
+class Square:
+    """Class defining a Square."""
+    def __init__(self, size=0):
+        """Initialise the square with a size."""
+        self.__size = size
+
+    def area(self):
+        """Returns current square area."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
