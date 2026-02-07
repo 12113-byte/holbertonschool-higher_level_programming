d97f0e637d8beafad7225ae0b5ef3fbc6d6a1862
Joleen <12113@holbertonstudents.com>
2026-02-07 12:16:06 +1100

    added documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""Module defining a class called Square."""
 class Square:
+    """Class defining a Square."""
     def __init__(self, size):
+        """Initialise the square with a size."""
         self.__size = size
