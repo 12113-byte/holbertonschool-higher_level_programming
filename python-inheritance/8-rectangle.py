5da746fec326e065d661da89dd349c991d95989d
Joleen <12113@holbertonstudents.com>
2026-02-15 18:34:02 +1100

    fixed pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,8 @@
 """Class called Rectangle inherits from BaseGeometry"""
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Class called Rectangle with private width and height validation"""
 
