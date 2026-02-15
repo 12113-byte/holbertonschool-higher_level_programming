c21cc219177d8df5acfcf6aa233e11ba1bafc0b1
Joleen <12113@holbertonstudents.com>
2026-02-15 17:17:38 +1100

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Method with a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """raises Exception with a message"""
+    def area(self):
+        raise Exception("area() is not implemented")
