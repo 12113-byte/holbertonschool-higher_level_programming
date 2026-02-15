2b40dd5b2068c2177cb1926cdb6bc520067d32a9
Joleen <12113@holbertonstudents.com>
2026-02-15 16:33:03 +1100

    pycodestyle fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Modules defines a class called MyList"""
+"""Module defines a class called MyList"""
 
 
 class MyList(list):
     """Class that inherits from list and adds method print_sorted"""
+
     def print_sorted(self):
         """Prints the list in ascending sorted order"""
         print(sorted(self))
