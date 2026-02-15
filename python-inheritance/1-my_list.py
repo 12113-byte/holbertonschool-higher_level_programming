af89ba52911841cea7aa4e6576d7838226ce899d
Joleen <12113@holbertonstudents.com>
2026-02-15 16:30:11 +1100

    pycodestyle fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,8 +3,7 @@
 
 
 class MyList(list):
-    """Class that inherits from list and adds
-    method print_sorted"""
+    """Class that inherits from list and adds method print_sorted"""
     def print_sorted(self):
         """Prints the list in ascending sorted order"""
         print(sorted(self))
