ebf5b5928cf510ff9ad7a90ab55a052ee68ac031
Joleen <12113@holbertonstudents.com>
2026-02-15 23:41:53 +1100

    fixed typo

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initialise square with size"""
-        self.integer_validation("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
