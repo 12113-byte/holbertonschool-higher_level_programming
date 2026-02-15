6f45033ae80954ddc3ee19adf866068dacf34ab1
Joleen <12113@holbertonstudents.com>
2026-02-15 16:47:34 +1100

    pycodestyle fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Function that checks if parameter is exactly an instance"""
+
 
 def is_same_class(obj, a_class):
     """Returns True, if obj is exactly an instance of a_class"""
