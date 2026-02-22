c66eba51a2bfaa6818da1d097878d946a0278739
Joleen <12113@holbertonstudents.com>
2026-02-22 19:38:48 +1100

    added documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,16 +3,19 @@
 and then saves them to a file"""
 
 import json
-from 5-save_to_json_file import save_to_json
+from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
+"""Loads existing list or create new one"""
 
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:
     items = []
+"""Adds command line arguments"""
 
 items.extend(sys.argv[1:])
+"""Saves updated list back to file"""
 
 save_to_json_file(items, filename)
