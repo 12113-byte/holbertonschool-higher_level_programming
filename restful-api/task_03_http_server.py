b88cc500d9b367d4eaa7241012b9c24140f2af5b
Joleen <12113@holbertonstudents.com>
2026-02-24 16:57:28 +1100

    Task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import http.server
+import json
+
+PORT = 8000
+
+
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Ok")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+server_address = ("", PORT)
+httpd = http.server.HTTPServer(server_address, Handler)
+print(f"Server running on port {PORT}.")
+httpd.serve_forever()
