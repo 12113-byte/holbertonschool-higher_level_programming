e6f81c713222e3b780ed7ea1e7030f015c18e9dd
Joleen <12113@holbertonstudents.com>
2026-03-01 18:04:50 +1100

    Task 5, password method changed

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -15,12 +15,12 @@ jwt = JWTManager(app)
 users = {
     "user1": {
         "username": "user1",
-        "password": generate_password_hash("password"),
+        "password": generate_password_hash("password", method="pbkdf2:sha256"),
         "role": "user"
         },
     "admin1": {
         "username": "admin1",
-        "password": generate_password_hash("password"),
+        "password": generate_password_hash("password", method="pbkdf2:sha256"),
         "role": "admin"
         }
 }
