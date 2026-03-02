eca3dff858212992f2cb1038b089776cacac18e9
Joleen <12113@holbertonstudents.com>
2026-03-02 07:11:28 +0000

    unsure

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,8 @@
 -- prints dexcription of a table form a database
-CREATE TABLE IF NOT EXISTS 'first_table' (
-    'id' INT NOT NULL AUTO_INCREMENT,
-    'name' VARCHAR(128) DEFAULT NULL,
-    'c' CHAR(1) DEFAULT NULL,
-    'created_at' DATE DEFAULT NULL,
-    PRIMARY KEY ('id')
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+first_table CREATE TABLE`first_table`(
+`id` int NOT NULL AUTO_INCREMENT,
+`name`varchar(128) DEFAULT NULL,
+`c`char(1) DEFAULT NULL,
+`created_at` date DEFAULT NULL,
+PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
