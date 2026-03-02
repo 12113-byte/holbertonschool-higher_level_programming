9825e133691429f2dbb8cf80944ac18343fcbe86
Joleen <12113@holbertonstudents.com>
2026-03-02 06:43:46 +0000

    typo

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- prints dexcription of a table form a database
 CREATE TABLE IF NOT EXISTS 'first_table' (
-    'id' iNT NOT NULL AUTO_INCREMENT,
+    'id' INT NOT NULL AUTO_INCREMENT,
     'name' VARCHAR(128) DEFAULT NULL,
     'c' CHAR(1) DEFAULT NULL,
     'created_at' DATE DEFAULT NULL,
