477c48d550dff1d7da102ec536414ed6a178dc81
Joleen <12113@holbertonstudents.com>
2026-03-02 06:41:51 +0000

    backticks

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,8 @@
 -- prints dexcription of a table form a database
-CREATE TABLE IF NOT EXISTS first_table (
-    id iNT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) DEFAULT NULL,
-    c CHAR(1) DEFAULT NULL,
-    created_at DATE DEFAULT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS 'first_table' (
+    'id' iNT NOT NULL AUTO_INCREMENT,
+    'name' VARCHAR(128) DEFAULT NULL,
+    'c' CHAR(1) DEFAULT NULL,
+    'created_at' DATE DEFAULT NULL,
+    PRIMARY KEY ('id')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
