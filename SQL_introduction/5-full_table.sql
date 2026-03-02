45d12123688ab63a31e72608e3f65a1abb72f01b
Joleen <12113@holbertonstudents.com>
2026-03-02 06:33:51 +0000

    Task 5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,10 @@
+-- prints dexcription of a table form a database
+SELECT COLUMN_NAME AS Field,
+        COLUMN_TYPE AS Type,
+        IS_NULLABLE AS Null,
+        COLUMN_KEY AS Key,
+        COLUMN_DEFAULT AS Default,
+        EXTRA AS Extra
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'first_table'; -- current database passed as argument
