0608f458c0f81c19f870902734aa2ab4e02117fc
Joleen <12113@holbertonstudents.com>
2026-03-06 00:50:55 +0000

    fixed and rewrote my code

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,3 @@
 -- prints description of a table from a database
-CREATE TABLE IF NOT EXISTS `first_table` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(128) DEFAULT NULL,
-  `c` CHAR(1) DEFAULT NULL,
-  `created_at` DATE DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SHOW CREATE TABLE first_table;
