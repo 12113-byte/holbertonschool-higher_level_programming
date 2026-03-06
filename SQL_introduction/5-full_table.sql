715c074b1af1e8f956c98120b14af7b02f7f7129
Joleen <12113@holbertonstudents.com>
2026-03-06 00:36:39 +0000

    added if not exist

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- prints description of a table from a database
-CREATE TABLE `first_table` (
+CREATE TABLE IF NOT EXISTS `first_table` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(128) DEFAULT NULL,
   `c` CHAR(1) DEFAULT NULL,
