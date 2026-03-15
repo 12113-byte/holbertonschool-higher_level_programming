0721d66f60ba39b149127dc753778ce4cfdf2edf
Joleen <12113@holbertonstudents.com>
2026-03-16 00:00:12 +1100

    Task 8 - added id to select

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all cities of California that can be found in the database hbtn_0d_usa
-SELECT name
+SELECT id, name
 FROM cities
 WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
