daa71e2a6ad86d18e7b749c7b66a6f9a60e70252
Joleen <12113@holbertonstudents.com>
2026-03-16 15:20:35 +1100

    Task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# importing dependencies sysarg, mysql connector
+import sys
+import MySQLdb
+# taking in args, assigning variables
+if len(sys.argv) != 5:
+    print(f"Usage: {sys.argv[0]} <db_user> <db_password> <db_table> <state>")
+    sys.exit(1)
+
+db_user = sys.argv[1]
+db_password = sys.argv[2]
+db_table = sys.argv[3]
+find_state = sys.argv[4]
+
+# simple database connection script
+# localhost connection port 3306
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=db_user,
+    passwd=db_password,
+    db=db_table,
+    charset="utf8"
+    )
+cur = conn.cursor()
+# execute sql query on target database
+cur.execute("SELECT * FROM states WHERE name EQUALS" +find_state+ "ORDER BY id ASC")
+# retrieving results as query_rows
+query_rows = cur.fetchall()
+# printing each row in query_rows e.g. results of db connection
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
