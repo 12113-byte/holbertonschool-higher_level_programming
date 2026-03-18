bb3e9222a2678e541a9e43b4c293c9769ce3dfb9
Joleen <12113@holbertonstudents.com>
2026-03-18 12:18:53 +1100

    changed 'name' to 'title'

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- importing database dump and list all Comedy shows
-SELECT tv_shows.title AS name
+SELECT tv_shows.title AS title
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY name ASC;
+ORDER BY title ASC;
