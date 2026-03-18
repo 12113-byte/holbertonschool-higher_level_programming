4e2c205bb5db9d33c8b1a2cc948335328c20a9e6
Joleen <12113@holbertonstudents.com>
2026-03-18 15:38:34 +1100

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Lists all states where name matches the argument"""
+# importing dependencies sysarg, mysql connector
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    # taking in args, assigning variables
+    if len(sys.argv) != 5:
+        print(
+            f"Usage: {sys.argv[0]} <db_user> <db_password> <db_name> <state>"
+            )
+        sys.exit(1)
+
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    find_state = sys.argv[4]
+
+    # simple database connection script
+    # localhost connection port 3306
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=db_user,
+        passwd=db_password,
+        db=db_name,
+        charset="utf8"
+        )
+    cur = conn.cursor()
+    # execute sql query on target database
+    cur.execute(
+        "SELECT * "
+        "FROM states "
+        "WHERE BINARY name = %s "
+        "ORDER BY id ASC", (find_state,)
+        )
+    # retrieving results as query_rows
+    query_rows = cur.fetchall()
+    # printing each row in query_rows e.g. results of db connection
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
