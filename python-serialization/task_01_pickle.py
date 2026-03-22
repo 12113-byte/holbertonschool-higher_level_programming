db5a43002d0e110370e39dbe44408dc26211c8d5
Joleen <12113@holbertonstudents.com>
2026-03-23 00:01:15 +1100

    added EOFError

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -24,5 +24,5 @@ class CustomObject:
         try:
             with open(filename, 'rb') as f:
                 return pickle.load(f)
-        except (FileNotFoundError, pickle.UnpicklingError):
+        except (FileNotFoundError, pickle.UnpicklingError, EOFError):
             return None
