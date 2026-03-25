74c1c03710e49f052affaef0862ab776f0d03365
Joleen <12113@holbertonstudents.com>
2026-03-25 15:46:58 +1100

    fixed no argument case

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -5,14 +5,10 @@ const args = process.argv.slice(2);
 const n = parseInt(args[0]);
 
 function factorial (n) {
-  if (n === 0 || n === 1 || isNaN(n)) {
+  if (n === 0 || n === 1 || isNaN(n) || args[0] === undefined) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
-if (args[0] === undefined) {
-  console.log('No argument');
-} else {
   console.log(factorial(n));
-}
