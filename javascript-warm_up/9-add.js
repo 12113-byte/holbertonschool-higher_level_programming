2e769b2713d31dba56dc836de3e67f419093360d
Joleen <12113@holbertonstudents.com>
2026-03-25 14:58:27 +1100

    Task 9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+// first arg is the first int
+const a = parseInt(args[0]);
+// second arg is second int
+const b = parseInt(args[1]);
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(a, b));
