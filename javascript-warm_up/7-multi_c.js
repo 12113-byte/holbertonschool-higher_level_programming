8bdb28c278b1383924bc0abe5e5c2c6a50dd6a90
Joleen <12113@holbertonstudents.com>
2026-03-25 14:25:43 +1100

    fixed semistandard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2);
 
-first_argument = args[0];
+const firstArgument = args[0];
 
-for (let i = 0; i < first_argument; i++) {
+for (let i = 0; i < firstArgument; i++) {
   console.log('C is fun');
 }
