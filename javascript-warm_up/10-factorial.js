781dbe8e32c3ec5a6b43881d50547d84ca39435c
Joleen <12113@holbertonstudents.com>
2026-03-25 15:45:02 +1100

    Task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,13 +2,17 @@
 
 const args = process.argv.slice(2);
 
-const firstArgument = parseInt(args[0]);
+const n = parseInt(args[0]);
 
-function factorial (firstArgument) {
-  if (firstArgument === 0 || firstArgument === 1) {
+function factorial (n) {
+  if (n === 0 || n === 1 || isNaN(n)) {
     return 1;
   }
-  return firstArgument * factorial(firstArgument - 1);
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(firstArgument));
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(factorial(n));
+}
