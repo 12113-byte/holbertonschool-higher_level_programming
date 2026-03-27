cba1b7d0c51eb3bb6c32e4e15ac589295701cccf
Joleen <12113@holbertonstudents.com>
2026-03-27 16:44:56 +1100

    Task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const numbers = args.map(arg => parseInt(arg));
+
+if (args.length === 1 || args.length === 0 || args === undefined) {
+  console.log(0);
+} else {
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
+}
